fdf5e1f2e43f33e7788b785c7ebade2aa8e97981
mercuri <mercuri90028@gmail.com>
2022-10-05 23:37:20 +0530

    done we can run

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Big Dream</h1>
+      <p data-ns-test="project-description">Become a FAther</p>
+    </div>
   )
 }
 
